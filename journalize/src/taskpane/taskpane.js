7741f18beccd6b5508dc755189c5e5372896e5f0
Dmytro <dpastov@gmail.com>
2021-11-23 13:16:55 +0100

    added a xhttp preoperty

--- journalize/src/taskpane/taskpane.js
+++ journalize/src/taskpane/taskpane.js
@@ -56,6 +56,7 @@
 
           var xhttp = new XMLHttpRequest();
           xhttp.open("POST", ewsurl, true);
+          xhttp.withCredentials = true;
           xhttp.setRequestHeader("Content-type", "text/xml");
           xhttp.setRequestHeader("Authorization", "Bearer " + token);
           xhttp.send(envelope);
