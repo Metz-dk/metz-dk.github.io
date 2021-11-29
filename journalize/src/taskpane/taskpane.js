09b43f8dc306cc25eccaad4f3851874820ac1641
Dmytro <dpastov@gmail.com>
2021-11-29 11:22:15 +0100

    test for graph

--- journalize/src/taskpane/taskpane.js
+++ journalize/src/taskpane/taskpane.js
@@ -94,7 +94,58 @@
         var app = docid.split('|')[0];
         var searchEl = $(".search-result");
 
-        searchEl.html("... preparing data ...");
+        searchEl.html("... sending data (please wait) ...");
+
+        // TEST: GRAPH
+        Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
+          if (result.status === "succeeded") {
+              var accessToken = result.value;
+              getCurrentItem(accessToken);
+          } else {
+              // Handle the error
+          }
+        });
+
+        function getCurrentItem(accessToken) {
+          // Get the item's REST ID
+          var itemId = getItemRestId();
+      
+          // Construct the REST URL to the current item
+          // Details for formatting the URL can be found at 
+          // https://msdn.microsoft.com/office/office365/APi/mail-rest-operations#get-a-message-rest
+         // var getMessageUrl = Office.context.mailbox.restUrl +
+         //     '/v2.0/me/messages/' + itemId;'
+           var PostURL = "https://outlook.office.com/api/beta/me/GetMailTips";
+           var mtipRequest = "{ \"EmailAddresses\": [ \"gscales@datarumble.com\" ],\"MailTipsOptions\": \"automaticReplies, mailboxFullStatus\"}";
+          //var getMessageUrl = "https://graph.microsoft.com/beta/me/messages/" + itemId;
+          $.ajax({
+              type: "POST",
+              contentType: "application/json; charset=utf-8",
+              url: PostURL,
+              data: mtipRequest,
+              dataType: 'json',
+              headers: { 'Authorization': 'Bearer ' + accessToken }
+          }).done(function (item) {
+              // Message is passed in `item`
+              var subject = item.value[0].EmailAddress.Address;
+              $('#ServerName').text("Message Subject : " + subject);
+          }).fail(function (error) {
+              // Handle error
+          });
+        }
+        
+        function getItemRestId() {
+            if (Office.context.mailbox.diagnostics.hostName === 'OutlookIOS') {
+                // itemId is already REST-formatted
+                return Office.context.mailbox.item.itemId;
+            } else {
+                // Convert to an item ID for API v2.0
+                return Office.context.mailbox.convertToRestId(
+                    Office.context.mailbox.item.itemId,
+                    Office.MailboxEnums.RestVersion.v2_0
+                );
+            }
+        }
 
         Office.context.mailbox.getCallbackTokenAsync(function(result) {
           var token = result.value;
@@ -124,7 +175,7 @@
           };
 
           xhttp.onerror = function() { // only triggers if the request couldn't be made at all
-            sendMemoError("Request failed (probably CORS)");
+            sendMemoError("Request failed");
           };
 
           function sendMemoError(txt) {
