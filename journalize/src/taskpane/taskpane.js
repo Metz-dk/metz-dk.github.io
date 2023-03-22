b041fc2ebe5d5815eba2e03ce6d287286f62c4af
Dmytro <dpastov@gmail.com>
2023-03-22 14:59:13 +0100

    keep focus on dropdown

--- journalize/src/taskpane/taskpane.js
+++ journalize/src/taskpane/taskpane.js
@@ -12,7 +12,7 @@
     
     // search
     jQuery(document).ready(function(){
-      $("form[name='search'] input").focus();
+      //$("form[name='search'] input").focus();
 
       // When user press on search button
       $("form[name='search']").on('submit', function(e){
